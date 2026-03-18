20a86229b418263fadfe77f629aeee592e657b0c
David Gayman <davidrgayman@gmail.com>
2026-03-18 01:04:18 -0400

    Adding a setup script.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://rustup.rs | sh
+source $HOME/.cargo/env
+
+sudo apt update
+sudo apt install \
+  clang libxkbcommon-x11-dev pkg-config libvulkan-dev \
+  libwayland-dev xorg-dev libxcb-shape0-dev libxcb-xfixes0-dev
+
+cargo install --path . --bin lapce --profile release-lto --locked
+
